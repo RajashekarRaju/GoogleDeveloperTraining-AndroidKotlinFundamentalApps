3759d189f95c97c03b80cdfbf414e3cf329632db
rajas <rajashekarraju123@gmail.com>
2020-05-12 23:50:01 +0530

    Added two modules to repository. BuildYourFirstApp, Layouts

---
+++ Lesson1-BuildYourFirstApp/src/main/java/com/developersbreach/buildyourfirstapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.developersbreach.buildyourfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.developersbreach.kotlinfundamentalapps.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

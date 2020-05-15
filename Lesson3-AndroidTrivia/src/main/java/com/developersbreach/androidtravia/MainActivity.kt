f33355bd105855afb6809e1650991c5af57c2bbe
rajas <rajashekarraju123@gmail.com>
2020-05-15 20:39:22 +0530

    Added starter code, completed TODO's, finished codelab lesson-3.1

--- Lesson3-AndroidTrivia/src/main/java/com/developersbreach/androidtravia/MainActivity.kt
+++ Lesson3-AndroidTrivia/src/main/java/com/developersbreach/androidtravia/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.developersbreach.androidtravia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.developersbreach.androidtravia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
---
+++ Lesson3-AndroidTrivia/src/main/java/com/developersbreach/androidtravia/TitleFragment.kt
@@ -0,0 +1,25 @@
+package com.developersbreach.androidtravia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.developersbreach.androidtravia.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+        return binding.root
+    }
+
+}

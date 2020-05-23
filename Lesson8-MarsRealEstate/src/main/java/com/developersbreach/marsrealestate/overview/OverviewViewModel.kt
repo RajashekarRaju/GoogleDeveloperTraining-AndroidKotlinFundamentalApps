7b44ef647aa117c986901c832e754ababcb3a923
rajas <rajashekarraju123@gmail.com>
2020-05-23 15:05:27 +0530

    Completed codelabs lesson 8.1: Getting data from the internet

--- Lesson8-MarsRealEstate/src/main/java/com/developersbreach/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson8-MarsRealEstate/src/main/java/com/developersbreach/marsrealestate/overview/OverviewViewModel.kt
@@ -3,6 +3,11 @@ package com.developersbreach.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.developersbreach.marsrealestate.network.MarsApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -16,6 +21,11 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(
+        viewModelJob + Dispatchers.Main
+    )
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -27,6 +37,19 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        coroutineScope.launch {
+            val getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+            try {
+                val listResult = getPropertiesDeferred.await()
+                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }

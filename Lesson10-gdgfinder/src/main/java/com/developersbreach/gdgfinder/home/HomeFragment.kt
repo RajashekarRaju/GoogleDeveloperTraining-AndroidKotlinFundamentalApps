f2c2409df22759a424336c8456c0ba509d3826d0
rajas <rajashekarraju123@gmail.com>
2020-05-24 08:01:13 +0530

    Added starter app code for lesson 10: GDG Finder

---
+++ Lesson10-gdgfinder/src/main/java/com/developersbreach/gdgfinder/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.developersbreach.gdgfinder.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.developersbreach.gdgfinder.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = HomeFragmentBinding.inflate(inflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        return binding.root
+    }
+}
